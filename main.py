b882261b8d04507c1d4c2ebb8a428588d27bac72
K7ens <>
2021-05-07 13:08:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+import os
+ 
+client = discord.Client()
+ 
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+ 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+ 
+    if message.content.startswith('Привет'):
+        await message.channel.send('Здарова!')
+    if message.content.startswith('пока'):
+        await message.channel.send('Ага давай!')
+ 
+client.run(os.environ['TOKEN'])
